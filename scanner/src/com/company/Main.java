77be0b565d779c7c97c48465ca4ba5504146ec4d
Mathieu <93948335+MathieuMDM@users.noreply.github.com>
2022-09-09 07:08:14 +0200

    Add files via upload

---
+++ scanner/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // next line character (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            int age = 2022 - yearOfBirth;
+
+            if(age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", your age is " + age);
+            } else {
+                System.out.println("Incorrect birth details");
+            }
+        } else {
+            System.out.println("Invalid date of birth");
+        }
+
+        scanner.close();
+    }
+}
